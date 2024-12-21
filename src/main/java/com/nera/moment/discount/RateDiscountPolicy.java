f82fb8a53c65ab958a1187eb78bb85537e71ccfc
nera <nera4936@gmail.com>
2024-12-21 19:36:32 +0900

    feature: add RateDiscountPolicy

---
+++ src/main/java/com/nera/moment/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package com.nera.moment.discount;
+
+import com.nera.moment.member.Grade;
+import com.nera.moment.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+
+}
---
+++ src/test/java/com/nera/moment/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,21 @@
+package com.nera.moment.discount;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.nera.moment.member.Grade;
+import com.nera.moment.member.Member;
+
+public class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        Member member = new Member(1L, "nera", Grade.VIP);
+        int discount = discountPolicy.discount(member, 10000);
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+}
