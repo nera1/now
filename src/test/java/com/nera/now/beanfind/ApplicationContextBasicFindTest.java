98931c8ed829acaaff0b2064291c7cf86023293b
nera1 <nera4936@gmail.com>
2025-01-03 20:17:51 +0900

    feature: add a test code to check Bean

---
+++ src/test/java/com/nera/now/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,28 @@
+package com.nera.now.beanfind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nera.now.AppConfig;
+import com.nera.now.service.MemberService;
+import com.nera.now.service.MemberServiceImpl;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Bean 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
